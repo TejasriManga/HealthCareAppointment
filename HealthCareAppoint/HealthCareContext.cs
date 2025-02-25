b744755809e09bd9cc4bac413a0f20931f9825f9
2388126 <2388126@LTIN522978>
2025-02-25 10:02:29 +0530

    commit 1

--- HealthCareAppoint/HealthCareContext.cs
+++ HealthCareAppoint/HealthCareContext.cs
@@ -13,21 +13,14 @@ namespace HealthCareAppoint
     public class HealthcareContext : DbContext
     {
         public DbSet<Appointment> Appointments { get; set; }
-        public DbSet<User> Users { get; set; }
+
+        public DbSet<>
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Encrypt=False");
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Appointment>()
-                .HasOne(a => a.User)
-                .WithMany(u => u.Appointment)
-                .HasForeignKey(u => u.UserId)
-                .OnDelete(DeleteBehavior.NoAction); // Change to NoAction
 
-
-        }
 
 
 
