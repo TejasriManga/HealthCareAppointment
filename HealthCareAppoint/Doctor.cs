6f3835b735dc23c2a99dc37150e7a1609da08632
Madipalli Gupta <2388224@cognizant.com>
2025-02-26 09:14:03 +0530

    change

---
+++ HealthCareAppoint/Doctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareAppoint
+{
+    public class Doctor
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int DoctorId { get; set; }
+
+        [Required]
+
+        public String DoctorName { get; set; } 
+
+        [Required]
+
+        public String DoctorSpecilization { get; set; } 
+
+        [Required]
+
+        public DateTime DateTime { get; set; }
+    }
+}
